3517944e4c5a1ef06d03b2075ca4c68405390587
Pablo <pgrobredo@gmail.com>
2021-10-08 17:10:41 +0200

    HTML Dashboard(Home)

--- frontend/Day-Post/daypost/src/index.js
+++ frontend/Day-Post/daypost/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Home></Home>
+        {/* <App /> */}
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
