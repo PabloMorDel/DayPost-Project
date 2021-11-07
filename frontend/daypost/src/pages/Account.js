3bf0d7be148d664345a47d80ff777183ec58e943
Pablo <pgrobredo@gmail.com>
2021-11-07 22:46:14 +0100

    CSS media querys Tablet

--- frontend/daypost/src/pages/Account.js
+++ frontend/daypost/src/pages/Account.js
@@ -12,91 +12,91 @@ import UserManager from '../components/UserManager';
 import useLocalStorage from '../hooks/useLocalStorage';
 
 function Account() {
-  const [token] = useContext(AuthContext);
-  const [loggedUserId] = useContext(UserIdContext);
-  const [currentUser, setCurrentUser] = useLocalStorage({}, 'currentUser');
-  const { setWaiting, setError } = useContext(StatusContext);
-  const [posts, setPosts] = useState([]);
-  // const { setWaiting, setError } = useContext(StatusContext);
-  useEffect(() => {
-    const urlUser = `http://localhost:4001/users/${loggedUserId}`;
-    const urlPosts = `http://localhost:4001/posts`;
+    const [token] = useContext(AuthContext);
+    const [loggedUserId] = useContext(UserIdContext);
+    const [currentUser, setCurrentUser] = useLocalStorage({}, 'currentUser');
+    const { setWaiting, setError } = useContext(StatusContext);
+    const [posts, setPosts] = useState([]);
+    // const { setWaiting, setError } = useContext(StatusContext);
+    useEffect(() => {
+        const urlUser = `http://localhost:4001/users/${loggedUserId}`;
+        const urlPosts = `http://localhost:4001/posts`;
 
-    getUser(
-      {
-        url: urlUser,
-        token,
-        onSuccess: (body) => {
-          setCurrentUser(body.message);
-        },
-      },
-      [setWaiting, setError]
+        getUser(
+            {
+                url: urlUser,
+                token,
+                onSuccess: (body) => {
+                    setCurrentUser(body.message);
+                },
+            },
+            [setWaiting, setError]
+        );
+        getPost({
+            url: urlPosts,
+            token,
+            onSuccess: (body) => {
+                console.log(body);
+                setWaiting(false);
+                setPosts(body.posts);
+            },
+            onError: (error) => {
+                setWaiting(false);
+                setError(error);
+            },
+        });
+    }, [loggedUserId, setCurrentUser, token, setError, setWaiting]);
+    console.log(currentUser);
+    // console.log(posts);
+
+    const userPosts = posts.filter(
+        (sameIdPost) => sameIdPost.idUser === loggedUserId
     );
-    getPost({
-      url: urlPosts,
-      token,
-      onSuccess: (body) => {
-        console.log(body);
-        setWaiting(false);
-        setPosts(body.posts);
-      },
-      onError: (error) => {
-        setWaiting(false);
-        setError(error);
-      },
-    });
-  }, [loggedUserId, setCurrentUser, token, setError, setWaiting]);
-  console.log(currentUser);
-  // console.log(posts);
+    // console.log(userPosts);
 
-  const userPosts = posts.filter(
-    (sameIdPost) => sameIdPost.idUser === loggedUserId
-  );
-  // console.log(userPosts);
-
-  return (
-    <div className='mainHomePage'>
-      <div className='navigator'>
-        {/* <<<<<<< HEAD */}
-        <NavigationBar />
-      </div>
-      <div className='userManager'>
-        <UserManager />
-        {/* ======= */}
-        <div className='userManager'>
-          <UserManager></UserManager>
+    return (
+        <div className='mainHomePage'>
+            <div className='navigator'>
+                {/* <<<<<<< HEAD */}
+                <NavigationBar />
+            </div>
+            <div className='userManager'>
+                <UserManager />
+                {/* ======= */}
+                <div className='userManager'>
+                    <UserManager></UserManager>
+                </div>
+                {/* >>>>>>> dervys */}
+            </div>
+            <div className='accountPageSearcher'>
+                <Searcher />
+            </div>
+            <div className='mainContent'>
+                <AccountCard />
+                <PostManager>
+                    {userPosts.length > 0
+                        ? userPosts.map((post) => {
+                              return (
+                                  <FeedPost
+                                      key={post.id}
+                                      topic={post.topic}
+                                      title={post.title}
+                                      likes={post.likes}
+                                  ></FeedPost>
+                              );
+                          })
+                        : 'No data'}
+                </PostManager>
+            </div>
+            <div className='aside'>
+                <div className='spotlightAccs'></div>
+                <div className='FAQ'></div>
+                <footer className='privacy'>
+                    <OutsideFooter />
+                </footer>
+            </div>
         </div>
-        {/* >>>>>>> dervys */}
-      </div>
-      <div className='accountPageSearcher'>
-        <Searcher />
-      </div>
-      <div className='mainContent'>
-        <AccountCard />
-        <PostManager>
-          {userPosts.length > 0
-            ? userPosts.map((post) => {
-                return (
-                  <FeedPost
-                    key={post.id}
-                    topic={post.topic}
-                    title={post.title}
-                    likes={post.likes}
-                  ></FeedPost>
-                );
-              })
-            : 'No data'}
-        </PostManager>
-      </div>
-      <div className='aside'>
-        <div className='spotlightAccs'></div>
-        <div className='FAQ'></div>
-        <footer className='privacy'>
-          <OutsideFooter />
-        </footer>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Account;
--- frontend/daypost/src/pages/Home.js
+++ frontend/daypost/src/pages/Home.js
@@ -13,76 +13,78 @@ import { PostCategories } from '../components/PostsCategories';
 import { Link } from '@mui/material';
 
 function Home(props) {
-  const { topic } = useParams();
-  const [token] = useContext(AuthContext);
-  const { setWaiting, setError } = useContext(StatusContext);
-  const [posts, setPosts] = useState([]);
-  //const [category, setCategory] = useState(null);
+    const { topic } = useParams();
+    const [token] = useContext(AuthContext);
+    const { setWaiting, setError } = useContext(StatusContext);
+    const [posts, setPosts] = useState([]);
+    //const [category, setCategory] = useState(null);
 
-  useEffect(() => {
-    const url = `http://localhost:4001/posts/${topic ? '?topic=' + topic : ''}`;
+    useEffect(() => {
+        const url = `http://localhost:4001/posts/${
+            topic ? '?topic=' + topic : ''
+        }`;
 
-    setWaiting(true);
+        setWaiting(true);
 
-    getPost({
-      url,
-      token,
-      onSuccess: (body) => {
-        console.log(body);
-        setWaiting(false);
-        setPosts(body.posts);
-      },
-      onError: (error) => {
-        setWaiting(false);
-        setError(error);
-      },
-    });
-  }, [token, setError, setWaiting, topic]);
+        getPost({
+            url,
+            token,
+            onSuccess: (body) => {
+                console.log(body);
+                setWaiting(false);
+                setPosts(body.posts);
+            },
+            onError: (error) => {
+                setWaiting(false);
+                setError(error);
+            },
+        });
+    }, [token, setError, setWaiting, topic]);
 
-  return (
-    <div className='mainHomePage'>
-      <div className='navigator'>
-        {/* <<<<<<< HEAD */}
-        <NavigationBar />
-      </div>
-      <div className='userManager'>
-        <UserManager />
-        {/* ======= */}
-        <div className='userManager'>
-          <UserManager></UserManager>
+    return (
+        <div className='mainHomePage'>
+            <div className='navigator'>
+                {/* <<<<<<< HEAD */}
+                <NavigationBar />
+            </div>
+            <div className='userManager'>
+                <UserManager />
+                {/* ======= */}
+                <div className='userManager'>
+                    <UserManager></UserManager>
+                </div>
+                {/* >>>>>>> dervys */}
+            </div>
+            <div className='mainContent'>
+                <Searcher />
+                <div className='contentHeader'></div>
+                <div className='postsNavBar'>
+                    <PostCategories />
+                </div>
+                <PostManager>
+                    {posts.length > 0
+                        ? posts.map((post) => {
+                              return (
+                                  <FeedPost
+                                      key={post.id}
+                                      id={post.id}
+                                      topic={post.topic}
+                                      title={post.title}
+                                      likes={post.likes}
+                                  ></FeedPost>
+                              );
+                          })
+                        : 'No data'}
+                </PostManager>
+            </div>
+            <div className='aside'>
+                <div className='spotlightAccs'></div>
+                <div className='FAQ'></div>
+                <footer className='privacy'>
+                    <OutsideFooter />
+                </footer>
+            </div>
         </div>
-        {/* >>>>>>> dervys */}
-      </div>
-      <div className='mainContent'>
-        <Searcher />
-        <div className='contentHeader'></div>
-        <div className='postsNavBar'>
-          <PostCategories />
-        </div>
-        <PostManager>
-          {posts.length > 0
-            ? posts.map((post) => {
-                return (
-                  <FeedPost
-                    key={post.id}
-                    id={post.id}
-                    topic={post.topic}
-                    title={post.title}
-                    likes={post.likes}
-                  ></FeedPost>
-                );
-              })
-            : 'No data'}
-        </PostManager>
-      </div>
-      <div className='aside'>
-        <div className='spotlightAccs'></div>
-        <div className='FAQ'></div>
-        <footer className='privacy'>
-          <OutsideFooter />
-        </footer>
-      </div>
-    </div>
-  );
+    );
 }
 export default Home;
