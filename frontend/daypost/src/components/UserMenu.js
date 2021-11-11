a2e8d5f73fbe337a2912003055f8fc03de7670c5
Pablo <pgrobredo@gmail.com>
2021-11-11 13:47:02 +0100

    css Home 1

--- frontend/daypost/src/components/UserMenu.js
+++ frontend/daypost/src/components/UserMenu.js
@@ -13,111 +13,114 @@ import { Avatar } from '@mui/material';
 import { useHistory } from 'react-router-dom';
 
 function CustomMenu(props) {
-  return (
-    <Menu
-      elevation={0}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'right',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      {...props}
-    />
-  );
+    return (
+        <Menu
+            elevation={0}
+            anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'right',
+            }}
+            transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+            }}
+            {...props}
+        />
+    );
 }
 function UserMenu({ avatar, userName }) {
-  const [token, setToken] = React.useContext(AuthContext);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const history = useHistory();
-  const routeChanger = () => {
-    const path = token ? `/account` : `/register`;
-    history.push(path);
-  };
-  const loginRouteChanger = () => {
-    const path = `/login`;
-    history.push(path);
-  };
-  const registerRouteChanger = () => {
-    const path = `/register`;
-    history.push(path);
-  };
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = (e) => {
-    e.preventDefault();
-    setAnchorEl(null);
-  };
-  const handleProfileButtonClick = (e) => {
-    e.preventDefault();
-    routeChanger();
-    setAnchorEl(null);
-  };
+    const [token, setToken] = React.useContext(AuthContext);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const history = useHistory();
+    const routeChanger = () => {
+        const path = token ? `/account` : `/register`;
+        history.push(path);
+    };
+    const loginRouteChanger = () => {
+        const path = `/login`;
+        history.push(path);
+    };
+    const registerRouteChanger = () => {
+        const path = `/register`;
+        history.push(path);
+    };
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = (e) => {
+        e.preventDefault();
+        setAnchorEl(null);
+    };
+    const handleProfileButtonClick = (e) => {
+        e.preventDefault();
+        routeChanger();
+        setAnchorEl(null);
+    };
 
-  const logOutOnClick = () => {
-    setAnchorEl(null);
-    setToken(null);
-  };
-  // console.log('Anchor', anchorEl);
+    const logOutOnClick = () => {
+        setAnchorEl(null);
+        setToken(null);
+    };
+    // console.log('Anchor', anchorEl);
 
-  return (
-    <div>
-      <Button
-        id='demo-customized-button'
-        aria-controls='demo-customized-menu'
-        aria-haspopup='true'
-        aria-expanded={open ? 'true' : undefined}
-        variant='contained'
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
-      >
-        {token ? (
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <Avatar src={avatar} style={{ height: '40px', width: '40px' }} />
-            {userName}
-          </div>
-        ) : (
-          'Hello, stranger!'
-        )}
-      </Button>
+    return (
+        <div className='user-menu'>
+            <Button
+                id='demo-customized-button'
+                aria-controls='demo-customized-menu'
+                aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
+                variant='contained'
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+            >
+                {token ? (
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Avatar
+                            src={avatar}
+                            style={{ height: '40px', width: '40px' }}
+                        />
+                        {userName}
+                    </div>
+                ) : (
+                    'Hello, stranger!'
+                )}
+            </Button>
 
-      <CustomMenu
-        id='demo-customized-menu'
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        {token ? (
-          <MenuItem onClick={handleProfileButtonClick} disableRipple>
-            Account
-          </MenuItem>
-        ) : (
-          <MenuItem onClick={registerRouteChanger} disableRipple>
-            Register
-          </MenuItem>
-        )}
-        {token ? (
-          <MenuItem onClick={logOutOnClick} disableRipple>
-            <LogoutIcon />
-            Log Out
-          </MenuItem>
-        ) : (
-          <MenuItem onClick={loginRouteChanger} disableRipple>
-            <LoginIcon />
-            Log In
-          </MenuItem>
-        )}
-      </CustomMenu>
-    </div>
-  );
+            <CustomMenu
+                id='demo-customized-menu'
+                MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+            >
+                {token ? (
+                    <MenuItem onClick={handleProfileButtonClick} disableRipple>
+                        Account
+                    </MenuItem>
+                ) : (
+                    <MenuItem onClick={registerRouteChanger} disableRipple>
+                        Register
+                    </MenuItem>
+                )}
+                {token ? (
+                    <MenuItem onClick={logOutOnClick} disableRipple>
+                        <LogoutIcon />
+                        Log Out
+                    </MenuItem>
+                ) : (
+                    <MenuItem onClick={loginRouteChanger} disableRipple>
+                        <LoginIcon />
+                        Log In
+                    </MenuItem>
+                )}
+            </CustomMenu>
+        </div>
+    );
 }
 
 export default UserMenu;
